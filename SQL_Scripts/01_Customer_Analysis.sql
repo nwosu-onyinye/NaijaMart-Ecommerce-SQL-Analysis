ed28e0ba106064dfbcb2ca08793d35969d48a61d
nwosu-onyinye <168085227+nwosu-onyinye@users.noreply.github.com>
2026-06-19 01:43:49 +0100

    Update 01_Customer_Analysis.sql

--- SQL_Scripts/01_Customer_Analysis.sql
+++ SQL_Scripts/01_Customer_Analysis.sql
@@ -1,1 +1,32 @@
+-- =====================================================
+-- CUSTOMER ANALYSIS
+-- Project: NaijaMart E-Commerce SQL Analysis
+-- Author: Nwosu Onyinyechi
+-- =====================================================
 
+-- Customer Segment Distribution
+
+SELECT
+    customer_segment,
+    COUNT(*) AS total_customers
+FROM customers
+GROUP BY customer_segment
+ORDER BY total_customers DESC;
+
+-- Customer Distribution by State
+
+SELECT
+    state,
+    COUNT(*) AS total_customers
+FROM customers
+GROUP BY state
+ORDER BY total_customers DESC;
+
+-- Average Customer Age by Segment
+
+SELECT
+    customer_segment,
+    ROUND(AVG(age),0) AS avg_age
+FROM customers
+GROUP BY customer_segment
+ORDER BY avg_age DESC;
