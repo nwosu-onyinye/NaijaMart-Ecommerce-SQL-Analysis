8d03af4f7c3393339ddef485724c576b050062c4
nwosu-onyinye <168085227+nwosu-onyinye@users.noreply.github.com>
2026-06-19 01:48:14 +0100

    Update 03_Product_Analysis.sql

--- SQL_Scripts/03_Product_Analysis.sql
+++ SQL_Scripts/03_Product_Analysis.sql
@@ -1,1 +1,49 @@
+-- =====================================================
+-- PRODUCT ANALYSIS
+-- Project: NaijaMart E-Commerce SQL Analysis
+-- Author: Nwosu Onyinyechi
+-- =====================================================
 
+-- Top 10 Products by Revenue
+
+SELECT
+    p.product_name,
+    ROUND(
+        SUM(
+            (p.selling_price * od.quantity)
+            - od.discount_amount
+        ),
+        2
+    ) AS revenue
+FROM products p
+JOIN order_details od
+    ON p.product_id = od.product_id
+GROUP BY p.product_name
+ORDER BY revenue DESC
+LIMIT 10;
+
+-- Revenue by Category
+
+SELECT
+    p.category,
+    ROUND(
+        SUM(
+            (p.selling_price * od.quantity)
+            - od.discount_amount
+        ),
+        2
+    ) AS revenue
+FROM products p
+JOIN order_details od
+    ON p.product_id = od.product_id
+GROUP BY p.category
+ORDER BY revenue DESC;
+
+-- Most Profitable Products
+
+SELECT
+    product_name,
+    profit_margin
+FROM products
+ORDER BY profit_margin DESC
+LIMIT 10;
